6418e16cab1580535e89e33bc10562af85e042ae
Heylon <heylon96@hotmail.com>
2021-01-11 21:31:25 +1000

    Use universal_newlines alias instead of text in runAndPrintOutput.py Compatible with the older version of python running on Google colab

--- runAndPrintOutput.py
+++ runAndPrintOutput.py
@@ -1,4 +1,4 @@
 import subprocess
 def runAndPrintOutput(arrayCommand:list):
-    result = subprocess.run(arrayCommand, shell=False, text=True, stderr=subprocess.STDOUT, check=False).stdout
+    result = subprocess.run(arrayCommand, shell=False, universal_newlines=True, stderr=subprocess.STDOUT, check=False).stdout
     print(result)
