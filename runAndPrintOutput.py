16e2b14c004beb28f95c29aafd2b886e1db298cc
Heylon <heylon96@hotmail.com>
2021-01-12 01:21:20 +1000

    Use popen instead of run to get stderr

--- runAndPrintOutput.py
+++ runAndPrintOutput.py
@@ -1,4 +1,6 @@
-import subprocess
+from subprocess import Popen, PIPE, STDOUT, run
 def runAndPrintOutput(arrayCommand:list):
-    result = subprocess.run(arrayCommand, shell=False, universal_newlines=True, stderr=subprocess.STDOUT, check=False).stdout
+    #result = run(arrayCommand, shell=False, universal_newlines=True, stderr=STDOUT, check=False).stdout
+    p = Popen(arrayCommand,shell=False,universal_newlines=True,stdin=PIPE, stdout=PIPE, stderr=STDOUT, close_fds=True)
+    result = p.stdout.read()
     print(result)
