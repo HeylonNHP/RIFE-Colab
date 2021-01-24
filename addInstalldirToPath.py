0e6629ed19aa8152a73639ef68eaa4f83b1947aa
Heylon <heylon96@hotmail.com>
2021-01-24 17:19:08 +1000

    Add dependency installer

---
+++ addInstalldirToPath.py
@@ -0,0 +1,7 @@
+import os
+import sys
+
+path = os.path.realpath(__file__)
+path = path[:path.rindex(os.path.sep)]
+print(path)
+sys.path.insert(0,path)
---
+++ install_dependencies.py
@@ -0,0 +1,77 @@
+import os
+import subprocess
+import sys
+import addInstalldirToPath
+
+REQUIRED_PACKAGES = ['numpy>=1.16', 'tqdm>=4.35.0', 'opencv-python>=4.1.2', 'pyqt5','requests']
+
+
+def install(package):
+    subprocess.check_call([sys.executable, "-m", "pip", "install", package])
+
+def downloadFile(url,outFile):
+    import requests
+    r = requests.get(url, allow_redirects=True)
+    open(outFile, 'wb').write(r.content)
+
+def extractArchive(archiveFile,exclusions:list = []):
+    sevenZip = "7z"
+    if os.name == 'nt':
+        sevenZip = r"C:\Program Files\7-Zip\7z.exe"
+    #os.system('"' + sevenZip + '"' + r' e '+'"'+ archiveFile +'"'+' -aoa')
+
+    sevenZipExclusions = []
+    for exclusion in exclusions:
+        sevenZipExclusions.append('-xr!' + exclusion)
+
+    subprocess.run([sevenZip,'e',archiveFile,'-aoa'] + sevenZipExclusions)
+
+def mainInstall():
+    for package in REQUIRED_PACKAGES:
+        install(package)
+
+    # Get torch
+    try:
+        import torch
+
+        versionString = torch.__version__
+        subversions = versionString.split('.')
+
+        if int(subversions[0]) < 1:
+            # Torch less than 1.0.0
+            raise Exception
+        if '+' not in subversions[2]:
+            # Torch CPU only version
+            raise Exception
+        print('Found torch', torch.__version__)
+    except:
+        # Install torch
+        print("Pytorch not found, getting 1.7.1 CUDA 11")
+        if os.name == 'nt':
+            # On windows
+            subprocess.check_call(
+                [sys.executable, '-m', 'pip', 'install', 'torch===1.7.1+cu110', 'torchvision===0.8.2+cu110',
+                 'torchaudio===0.7.2', '-f', 'https://download.pytorch.org/whl/torch_stable.html'])
+        else:
+            # On linux
+            subprocess.check_call(
+                [sys.executable, '-m', 'pip', 'install', 'torch==1.7.1+cu110', 'torchvision==0.8.2+cu110',
+                 'torchaudio===0.7.2', '-f', 'https://download.pytorch.org/whl/torch_stable.html'])
+
+
+    # Check ffmpeg
+    ffmpegExists = False
+    ffprobeExists = False
+    while not ffmpegExists or not ffprobeExists:
+        try:
+            subprocess.run(['ffmpeg'])
+            ffmpegExists = True
+            subprocess.run(['ffprobe'])
+            ffprobeExists = True
+        except:
+            print("Can't find ffmpeg")
+            downloadFile(r'https://github.com/BtbN/FFmpeg-Builds/releases/download/autobuild-2021-01-23-12-48/ffmpeg-n4.3.1-29-g89daac5fe2-win64-gpl-4.3.zip','ffmpeg.zip')
+            extractArchive('ffmpeg.zip',['doc'])
+
+
+mainInstall()
