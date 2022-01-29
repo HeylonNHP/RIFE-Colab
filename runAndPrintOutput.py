a364857c79bb4de58c8cf0bbb4e49fcf8bdd12a8
Heylon White <heylon96@hotmail.com>
2022-01-29 20:31:31 +1000

    Refactor runAndPrintOutput.py

--- runAndPrintOutput.py
+++ runAndPrintOutput.py
@@ -1,9 +1,9 @@
 from subprocess import Popen, PIPE, STDOUT, run
 
 
-def run_and_print_output(arrayCommand: list):
+def run_and_print_output(array_command: list):
     # result = run(arrayCommand, shell=False, universal_newlines=True, stderr=STDOUT, check=False).stdout
-    p = Popen(arrayCommand, shell=False, universal_newlines=True, stdin=PIPE, stdout=PIPE, stderr=STDOUT,
+    p = Popen(array_command, shell=False, universal_newlines=True, stdin=PIPE, stdout=PIPE, stderr=STDOUT,
               close_fds=True)
     result = p.stdout.read()
     print(result)
