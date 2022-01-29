b71599339008a859606ae10daf5e31c4c83eec7e
Heylon White <heylon96@hotmail.com>
2022-01-29 20:45:43 +1000

    Refactor runInterpolationAllSteps.py

--- runInterpolationAllSteps.py
+++ runInterpolationAllSteps.py
@@ -1,12 +1,24 @@
 from str2bool import *
 import argparse
+
+import os
+import sys
+import addInstalldirToPath
+from Globals.EncoderConfig import EncoderConfig
+
+# Why should we need to add the submodule to the path, just for the RIFE import to work
+# Thanks for being consistently terrible, python
+sys.path.insert(0, os.getcwd() + os.path.sep + 'arXiv2020RIFE')
+print(sys.path)
+
 parser = argparse.ArgumentParser(description='Interpolation for video input')
 parser.add_argument('-i', dest='inputFile', type=str, default=None)
 parser.add_argument('-if', dest='interpolationFactor', type=int, default=2)
-parser.add_argument('-targetfpsmode',dest='mode3targetfpsenabled',type=str2bool,default=False)
-parser.add_argument('-targetfps',dest='mode3targetfps',type=float,default=60)
-parser.add_argument('-maxBatchBackupThreadRestarts', dest='maxBatchBackupThreadRestarts',type=int)
-parser.add_argument('-exitOnMaxBatchBackupThreadRestarts', dest='exitOnMaxBatchBackupThreadRestarts',type=str2bool,default=False)
+parser.add_argument('-targetfpsmode', dest='mode3targetfpsenabled', type=str2bool, default=False)
+parser.add_argument('-targetfps', dest='mode3targetfps', type=float, default=60)
+parser.add_argument('-maxBatchBackupThreadRestarts', dest='maxBatchBackupThreadRestarts', type=int)
+parser.add_argument('-exitOnMaxBatchBackupThreadRestarts', dest='exitOnMaxBatchBackupThreadRestarts', type=str2bool,
+                    default=False)
 parser.add_argument('-loop', dest='loopable', type=str2bool, default=False)
 parser.add_argument('-mode', dest='mode', type=int, default=3)
 parser.add_argument('-crf', dest='crfout', type=int, default=20)
@@ -21,22 +33,13 @@ parser.add_argument('-autoencode', dest='autoencode', type=str2bool, default=Fal
 parser.add_argument('-blocksize', dest='blocksize', type=int, default=3000)
 args = parser.parse_args()
 
-import os
-import sys
-import addInstalldirToPath
-from Globals.EncoderConfig import EncoderConfig
-
-# Why should we need to add the submodule to the path, just for the RIFE import to work
-# Thanks for being consistently terrible, python
-sys.path.insert(0, os.getcwd() + os.path.sep + 'arXiv2020RIFE')
-print(sys.path)
-
 from generalInterpolationProceedures import *
-#setupRIFE(os.getcwd(),args.gpuid)
+
+# setupRIFE(os.getcwd(),args.gpuid)
 selectedGPUs = str(args.gpuid).split(",")
 selectedGPUs = [int(i) for i in selectedGPUs]
 
-setGPUinterpolationOptions(args.batchSize,selectedGPUs)
+setGPUinterpolationOptions(args.batchSize, selectedGPUs)
 
 encoderConfig = EncoderConfig()
 encoderConfig.setEncodingCRF(float(args.crfout))
@@ -52,11 +55,11 @@ interpolatorConfig.setClearPngs(args.clearpngs)
 interpolatorConfig.setLoopable(args.loopable)
 
 if args.mode == 3 and args.mode3targetfpsenabled == True:
-    interpolatorConfig.setMode3TargetFPS(True,args.mode3targetfps)
+    interpolatorConfig.setMode3TargetFPS(True, args.mode3targetfps)
 else:
     interpolatorConfig.setInterpolationFactor(args.interpolationFactor)
 
-if(args.maxBatchBackupThreadRestarts is not None):
+if (args.maxBatchBackupThreadRestarts is not None):
     interpolatorConfig.setBackupThreadStartLimit(args.maxBatchBackupThreadRestarts)
 interpolatorConfig.setExitOnBackupThreadLimit(args.exitOnMaxBatchBackupThreadRestarts)
 
