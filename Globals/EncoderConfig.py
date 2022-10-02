9a5b95c8e29138cfdf4fba3ff90742472ee26e46
Heylon White <heylon96@hotmail.com>
2022-10-02 16:36:33 +1000

    Use private naming convention in EncoderConfig.py

--- Globals/EncoderConfig.py
+++ Globals/EncoderConfig.py
@@ -1,24 +1,24 @@
 class EncoderConfig:
-    nvencGPUID = 0
-    useNvenc = False
-    useH265 = False
-    encodingPreset = "veryslow"
-    encodingProfile = "high"
-    encodingCRF = 20
-    pixelFormat = "yuv420p"
+    _nvencGPUID = 0
+    _useNvenc = False
+    _useH265 = False
+    _encodingPreset = "veryslow"
+    _encodingProfile = "high"
+    _encodingCRF = 20
+    _pixelFormat = "yuv420p"
 
-    availableProfilex264 = ['baseline','main','high','high444p']
-    availableProfilex265 = ['main','main10']
-    availableEncodingPresetsx26x = ['ultrafast','superfast','veryfast','faster','fast','medium','slow','slower','veryslow','placebo']
-    availableEncodingPresetsNvenc = ['fast','medium','slow','lossless']
-    crfRange = [0,51]
+    _availableProfilex264 = ['baseline', 'main', 'high', 'high444p']
+    _availableProfilex265 = ['main', 'main10']
+    _availableEncodingPresetsx26x = ['ultrafast', 'superfast', 'veryfast', 'faster', 'fast', 'medium', 'slow', 'slower', 'veryslow', 'placebo']
+    _availableEncodingPresetsNvenc = ['fast', 'medium', 'slow', 'lossless']
+    _crfRange = [0, 51]
 
-    enableFFmpegOutputFPS = False
-    FFmpegOutputFPS = 60
+    _enableFFmpegOutputFPS = False
+    _FFmpegOutputFPS = 60
 
     # Looping
-    preferredLoopLength = 10
-    maxLoopLength = 15
+    _preferredLoopLength = 10
+    _maxLoopLength = 15
     # Enable looping the output
     loopRepetitionsEnabled = True
 
@@ -26,83 +26,83 @@ class EncoderConfig:
         pass
 
     def setLoopingOptions(self,preferredLength:float, maxLength:float, loopingEnabled:bool):
-        self.preferredLoopLength = preferredLength
-        self.maxLoopLength = maxLength
+        self._preferredLoopLength = preferredLength
+        self._maxLoopLength = maxLength
         self.loopRepetitionsEnabled = loopingEnabled
 
     def getLoopingOptions(self):
-        return [self.preferredLoopLength,self.maxLoopLength,self.loopRepetitionsEnabled]
+        return [self._preferredLoopLength, self._maxLoopLength, self.loopRepetitionsEnabled]
 
     def setNvencGPUID(self,gpuid:int):
         if not gpuid < 0:
-            self.nvencGPUID = gpuid
+            self._nvencGPUID = gpuid
         else:
             raise Exception("GPU ID out of range")
 
     def enableNvenc(self,enable:bool):
-        self.useNvenc = enable
+        self._useNvenc = enable
 
     def enableH265(self,enable:bool):
-        self.useH265 = enable
+        self._useH265 = enable
 
     def setEncodingPreset(self,preset:str):
-        if self.useNvenc:
-            if preset not in self.availableEncodingPresetsNvenc:
+        if self._useNvenc:
+            if preset not in self._availableEncodingPresetsNvenc:
                 raise Exception("Preset doesn't exist")
         else:
-            if preset not in self.availableEncodingPresetsx26x:
+            if preset not in self._availableEncodingPresetsx26x:
                 raise Exception("Preset doesn't exist")
-        self.encodingPreset = preset
+        self._encodingPreset = preset
 
     def setEncodingProfile(self,profile:str):
-        if self.useH265:
-            if not profile in self.availableProfilex265:
+        if self._useH265:
+            if not profile in self._availableProfilex265:
                 raise Exception("Profile doesn't exist")
         else:
-            if not profile in self.availableProfilex264:
+            if not profile in self._availableProfilex264:
                 raise Exception("Profile doesn't exist")
-        self.encodingProfile = profile
+        self._encodingProfile = profile
 
     def setEncodingCRF(self,crf:float):
-        if crf > self.crfRange[1] or crf < self.crfRange[0]:
+        if crf > self._crfRange[1] or crf < self._crfRange[0]:
             raise Exception("CRF out of range")
-        self.encodingCRF = crf
+        self._encodingCRF = crf
 
     def setPixelFormat(self,pixelFormat:str):
-        self.pixelFormat = pixelFormat
+        self._pixelFormat = pixelFormat
 
     def setFFmpegOutputFPS(self,enable:bool,value:float):
-        self.enableFFmpegOutputFPS = enable
-        self.FFmpegOutputFPS = value
+        self._enableFFmpegOutputFPS = enable
+        self._FFmpegOutputFPS = value
 
 
     def getNvencGPUID(self):
-        return self.nvencGPUID
+        return self._nvencGPUID
     def nvencEnabled(self):
-        return self.useNvenc
+        return self._useNvenc
     def h265Enabled(self):
-        return self.useH265
+        return self._useH265
     def getEncodingPreset(self):
-        return self.encodingPreset
+        return self._encodingPreset
     def getEncodingProfile(self):
-        return self.encodingProfile
+        return self._encodingProfile
     def getEncodingCRF(self):
-        return self.encodingCRF
+        return self._encodingCRF
     def getPixelFormat(self):
-        return self.pixelFormat
+        return self._pixelFormat
     def getEncoder(self):
-        if self.useNvenc:
-            if not self.useH265:
+        if self._useNvenc:
+            if not self._useH265:
                 return 'h264_nvenc'
             else:
                 return 'hevc_nvenc'
         else:
-            if not self.useH265:
+            if not self._useH265:
                 return 'libx264'
             else:
                 return 'libx265'
 
     def FFmpegOutputFPSEnabled(self):
-        return self.enableFFmpegOutputFPS
+        return self._enableFFmpegOutputFPS
     def FFmpegOutputFPSValue(self):
-        return self.FFmpegOutputFPS
+        return self._FFmpegOutputFPS
